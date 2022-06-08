6368d39534e1d63fe131f8be8fc65171eef3a416
BBeak <bkg3441@gmail.com>
2022-06-08 19:45:17 +0900

    호출시 반환되는 값이 1씩 커지는 컨트롤러 액션 구현

--- src/main/java/com/bkh/exam/demo/controller/UserHomeController.java
+++ src/main/java/com/bkh/exam/demo/controller/UserHomeController.java
@@ -1,29 +1,37 @@
 package com.bkh.exam.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserHomeController {
+	int num = 0;
 	@RequestMapping("usr/home/main")
 	@ResponseBody
 	public String showMain() {
 		return "안녕하세요";
 	}
-	
 
 	@RequestMapping("usr/home/main2")
 	@ResponseBody
 	public String showMain2() {
 		return "반갑습니다.";
-}
+	}
 
-
-@RequestMapping("usr/home/main3")
-@ResponseBody
-public String showMain3() {
-	return "또 만나요";
-}
+	@RequestMapping("usr/home/main3")
+	@ResponseBody
+	public String showMain3() {
+		return "또 만나요";
+	}
+	@RequestMapping("usr/home/main4")
+	@ResponseBody
+	public int showMain4() {
+		
+		return num--;
+	}
 
 }
