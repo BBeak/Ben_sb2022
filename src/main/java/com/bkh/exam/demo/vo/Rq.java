99f7c6b025643b1f9a7bfbc0790e1af69fc8b16c
Kim-dong-gn <yu01071528097@gmail.com>
2022-07-12 21:48:35 +0900

    Rq를 통한 중복 제거

---
+++ src/main/java/com/bkh/exam/demo/vo/Rq.java
@@ -0,0 +1,26 @@
+package com.bkh.exam.demo.vo;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import lombok.Getter;
+
+public class Rq {
+	
+	@Getter
+	private boolean isLogined;
+	@Getter
+	private int loginedMemberId;
+	
+	public Rq(HttpServletRequest req) {
+		HttpSession httpSession = req.getSession();
+		
+		if (httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+		}
+		
+		this.isLogined = isLogined;
+		this.loginedMemberId = loginedMemberId;
+	}
+}
